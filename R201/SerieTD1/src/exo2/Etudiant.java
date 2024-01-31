c2b6bb2e214c240ae2e20c2b115cb63dd1a5cd44
MBESNARD <90607849+MBESNARD99@users.noreply.github.com>
2024-01-31 15:02:22 +0100

    Add files via upload

--- R201/SerieTD1/src/exo2/Etudiant.java
+++ R201/SerieTD1/src/exo2/Etudiant.java
@@ -1,56 +1,117 @@
 package exo2;
 
 public class Etudiant {
-	private static final String FIRSTNAME_DEFAULT = "NO-FIRSTNAME";
-	private static final String NAME_DEFAULT = "NO-NAME";
-	private static final String NUM_DEFAULT = "NO-NUM";
+
+	// constantes
+	private static final String FIRSTNAME = "NO-FIRSTNAME";
+	private static final String LASTNAME = "NO-NAME";
 	private static final String DPT_DEFAULT = "NON-COMMUNIQUÉ";
+	private static final String ANNEE = "2023";
+
+	// attributs de classe
+	private static int cptEtud = 0;
+
+	// attributs d'objets
 	public String numero;
 	public String nom;
 	public String prenom;
 	public String departement;
 	public int nbAbs;
-	
+
 	public Etudiant() {
-		this(NUM_DEFAULT, NAME_DEFAULT, FIRSTNAME_DEFAULT);
-	}
-	
-	public Etudiant(String numero, String nom, String prenom) {
-		this(numero, nom, prenom, DPT_DEFAULT);
+		this(LASTNAME, FIRSTNAME);
 	}
 
-	public Etudiant(String numero, String nom, String prenom, String departement) {
-		this.numero = numero;
+	public Etudiant(String nom, String prenom) {
+		this(nom, prenom, DPT_DEFAULT);
+	}
+
+	public Etudiant(String nom, String prenom, String departement) {
+		this.numero = ANNEE + "-" + ++cptEtud;
 		this.nom = nom;
 		this.prenom = prenom;
 		this.departement = departement;
 	}
 
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getDepartement() {
+		return departement;
+	}
+
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
+
+	public int getNbAbs() {
+		return nbAbs;
+	}
+
+	public void setNbAbs(int nbAbs) {
+		this.nbAbs = nbAbs;
+	}
+
+	public static String getFirstnameDefault() {
+		return FIRSTNAME;
+	}
+
+	public static String getNameDefault() {
+		return LASTNAME;
+	}
+
+	public static String getDptDefault() {
+		return DPT_DEFAULT;
+	}
+
 	public void afficherInformations() {
-		System.out.println("[" + numero + "] "
-				+ nom + " "
-				+ prenom + "(département "
-				+ departement + ") - nb d'abs : "
-				+ nbAbs);
+		System.out.println(
+				"[" + numero + "] " + nom + " " + prenom + "(département " + departement + ") - nb d'abs : " + nbAbs);
 	}
-	
+
 	public String toString() {
-		return "[" + numero + "] "
-				+ nom + " "
-				+ prenom + "(département "
-				+ departement + ") - nb d'abs : "
-				+ nbAbs;
+		return "[" + numero + "] " + nom + " " + prenom + "(département " + departement + ") - nb d'abs : " + nbAbs;
 	}
-	
+
 	public void ajouterAbsence() {
 		nbAbs++;
 	}
-	
+
 	public void enleverAbsence() {
 		nbAbs--;
 	}
-	
+
 	public void modifierNom(String nouveauNom) {
 		nom = nouveauNom;
+	}
+	
+	public boolean egaleA(Etudiant autreEtudiant) {
+		if (autreEtudiant == null)
+			return false;
+		return nom.equals(autreEtudiant.nom)
+				&& prenom.equals(autreEtudiant.prenom)
+				&& departement.equals(autreEtudiant.departement)
+				&& nbAbs == autreEtudiant.nbAbs;
 	}
 }
--- R201/SerieTD1/src/exo2/GererLesEtudiants.java
+++ R201/SerieTD1/src/exo2/GererLesEtudiants.java
@@ -3,20 +3,22 @@ package exo2;
 public class GererLesEtudiants {
 
 	public static void main(String[] args) {
-		
-		Etudiant etudiant1 = new Etudiant();
-		System.out.println(etudiant1);
-		System.out.println(etudiant1.nbAbs);
-		
-		etudiant1.afficherInformations();
-		etudiant1.ajouterAbsence();
-		etudiant1.afficherInformations();
-      	
-      	Etudiant etudiant2= new Etudiant("2022-2", "Dubois","Jean");
-    	Etudiant etudiant3= new Etudiant("2022-3", "Durand","Guillaume","INFO");
-    	
-    	etudiant2.afficherInformations();
-    	etudiant3.afficherInformations();
+
+//		Etudiant etudiant1 = new Etudiant();
+//		System.out.println(etudiant1);
+//		System.out.println(etudiant1.getNbAbs());
+//
+//		etudiant1.afficherInformations();
+//		etudiant1.ajouterAbsence();
+//		etudiant1.afficherInformations();
+//
+//		Etudiant etudiant2 = new Etudiant("Dubois", "Jean");
+		Etudiant etudiant3 = new Etudiant("Durand", "Guillaume", "INFO");
+		Etudiant etudiant4 = new Etudiant("Durand", "Guillaume", "INFO");
+		System.out.println(etudiant3);
+		System.out.println(etudiant4);
+
+		System.out.println(etudiant3.egaleA(etudiant4));
 
 	}
 
